ef959a4ae7b0247f3e11eb726efc7d5e63811bb5
Herny664 <hern4n.roch0n@gmail.com>
2024-03-02 16:11:06 -0300

    Creación de función para generar QRs

---
+++ QR_generador.py
@@ -0,0 +1,18 @@
+import qrcode
+import streamlit as st
+
+filename = "QR_codes/QR_code.png"
+
+def generate_qr_code(url, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
